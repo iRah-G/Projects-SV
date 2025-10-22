8901061d2156939908634ca39583f31be515add2
Rahi Gaikwad <rahigaikwad@n01-zeus.olympus.ece.tamu.edu>
2025-10-22 18:54:10 -0500

    Update xrun.f

---
+++ run.f
@@ -0,0 +1,6 @@
++access+rwc                   //allow probes to record signals
+////-gui
+-timescale 1ns/1ns            //set simulation time precision
+
+top.sv
+tb.sv
---
+++ xcelium.d/run.lnx86.22.03.d/.timestamp.ts
@@ -0,0 +1,2 @@
+1761176801 /home/grads/r/rahigaikwad/project/Projects-SV/tb.sv
+1761176801 /home/grads/r/rahigaikwad/project/Projects-SV/top.sv
